49908f2c07b52ba809dc6fe1d4e16a84799431b3
Wierdtangaming <wierdtangaming@gmail.com>
2020-01-29 16:20:17 +0530

    Second level

---
+++ Ball.js
@@ -0,0 +1,30 @@
+class Ball{
+  constructor(x, y, width, height,r,g,b,angle) {
+      var options = {
+          'restitution':0.0,
+          'friction':1.0,
+          'density':10.
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+
+      World.add(world, this.body);
+      this.r = r;
+      this.g = g;
+      this.b = b;
+    }
+    display(){
+
+      rgbcount = 1;
+      var angle = this.body.angle;
+      push();
+      strokeWeight(1);
+      fill(this.r,this.g,this.b);
+      translate(this.body.position.x, this.body.position.y);
+      rotate(angle);
+    //    rect(0,0,this.width,this.height );
+      ellipse(0,0,this.width,this.height);
+      pop();
+    }
+}
